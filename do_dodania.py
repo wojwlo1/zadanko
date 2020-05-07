37b7d1c4f793389a7c2a53a9abc0f49482b242e9
wojwlo1 <64958698+wojwlo1@users.noreply.github.com>
2020-05-07 12:49:53 +0200

    cennik
    program wyświetlający ceny zapisanych w słowniku produktów

---
+++ do_dodania.py
@@ -0,0 +1,11 @@
+# def ceny (cennik, produkt):
+#     if produkt in cennik:
+#         return cennik[produkt]
+#     else:
+#         return 0
+#
+# cennik = {"woda": 2, "chleb": 3, "komputer": 5000, "roślina": 20}
+#
+# for i in range (3):
+#     produkt = input("Cenę którego produktu chcesz poznać?")
+#     print(ceny(cennik, produkt))
