6d5d387c066e09b0e8bf3d2d8ca60dd22c70e4e3
jedrzejwasilewski <64958552+jedrzejwasilewski@users.noreply.github.com>
2020-05-15 15:41:28 +0200

    Update to cennik(jedwas)
    Hello mr wojwlo1 i updated your "cennik" so now it shows what products a customer can buy in your shop and the function returns summed price of selected products. (sorry for bad english im from POLAND :D)

--- do_dodania.py
+++ do_dodania.py
@@ -1,14 +1,16 @@
+#jedwas co odjebal
 def ceny (cennik, produkt):
   if produkt in cennik:
     return cennik[produkt]
   else:
     return 0
-
+suma=0
 cennik = {"woda": 2, "chleb": 3, "komputer": 5000, "roślina": 20, "piwko": 3, "papieroski": 16}
-
+print("produkty:\nwoda\nchleb\nkomputer\nroslina\npiwko\npapieroski")
 ilosc_produktow = int(input("Ceny ilu produktów chcesz sprawdzić?")) #zapytanie o to ile cen produktow sprawdzic
 for i in range (ilosc_produktow):
     produkt = input("Cenę którego produktu chcesz poznać?")
+    suma+=cennik[produkt]
     print(ceny(cennik, produkt))
-
+print("Całość za zakupy:",suma,"złotych")  
 
